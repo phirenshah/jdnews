474d61b7cda6aaa3ac989630c05c91bf7ccad3db
Keyan Shah <agentk1710@gmail.com>
2025-12-12 19:06:33 +0000

    Move them little to the pointing arrows.

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -155,13 +155,13 @@ export function Header({ lang }: { lang: string }) {
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-16 items-center">
         
-        <div className="mr-6 flex items-center">
+        <div className="flex items-center">
           <Link href={`/${lang}`} className="flex items-center">
             <Image src="/logo.png" alt="JD News Logo" width={120} height={0} style={{height: 'auto'}} />
           </Link>
         </div>
         
-        <nav className="hidden md:flex items-center space-x-6 text-sm font-medium">
+        <nav className="hidden md:flex items-center space-x-6 text-sm font-medium ml-6">
           {navLinks.map((link) => (
             <Link
               key={link.name}
