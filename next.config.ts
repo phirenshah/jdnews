42e314c91a6957a9d61f026f81d9493f01e5823e
Keyan Shah <agentk1710@gmail.com>
2025-12-13 17:16:48 +0000

    Try fixing this error: `Runtime Error: Invalid src prop (http://static.g

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -31,6 +32,12 @@ const nextConfig: NextConfig = {
       {
         protocol: 'https',
         hostname: 'i.ibb.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'static.gujaratsamachar.com',
         port: '',
         pathname: '/**',
       }
