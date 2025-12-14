898ecdde010c1f40a8780d3a393bbd745a775beb
Keyan Shah <agentk1710@gmail.com>
2025-12-14 16:09:27 +0000

    Fix utomatic initialization failed. Falling back to firebase config obje

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -8,16 +8,8 @@ import { getFirestore } from 'firebase/firestore'
 // IMPORTANT: DO NOT MODIFY THIS FUNCTION
 export function initializeFirebase() {
   if (!getApps().length) {
-    let firebaseApp;
-    try {
-      firebaseApp = initializeApp();
-    } catch (e) {
-      if (process.env.NODE_ENV === "production") {
-        console.warn('Automatic initialization failed. Falling back to firebase config object.', e);
-      }
-      firebaseApp = initializeApp(firebaseConfig);
-    }
-
+    // Always initialize with the config object for consistent behavior across all environments.
+    const firebaseApp = initializeApp(firebaseConfig);
     return getSdks(firebaseApp);
   }
 
